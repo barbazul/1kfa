89c63a463f96de6eaa3c9cf5e4c8c6592df998ea
Shandy Brown <github@ezide.com>
2019-02-20 16:21:03 -0800

    Add git hooks scripts
    Former-commit-id: 6c5ce4345fda91528beaaa2910491ac2d3e63042 [formerly fbee7af547e3c1c00925b922ba260e5882f217f1]
    Former-commit-id: 122fc50480e35e31b5b3eabf4dd3ef5fd2258c32

---
+++ bin/githook-commit-msg.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python2
+
+#
+# A hook script called by .git/hooks/commit-msg
+#
+
+import os, sys
+import subprocess
+
+
+
+dot_git_hooks_commit_msg = '''
+#!/bin/sh
+#
+# .git/hooks/commit-msg
+#
+# A hook script to check the commit log message.
+# Called by "git commit" with one argument, the name of the file
+# that has the commit message.  The hook should exit with non-zero
+# status after issuing an appropriate message if it wants to stop the
+# commit.  The hook is allowed to edit the commit message file.
+#
+
+echo "PWD is $PWD"
+./bin/githook-commit-msg.py $1
+'''
+
+
+c = open(sys.argv[1]).read()
+print 'got a file of length', len(c)
+
+lines = [
+    l for l in c.split('\n')
+    if not (l.startswith('#') or l.strip() == '')
+]
+message = ' '.join(lines)
+print 'message was', message
+tweet = (
+    'https://twitter.com/home?status='
+    + '%23ttrpg %23gamedesign New update to https://www.1kfa.com/table - '
+    + message
+).replace(' ', '+')
+
+print ''
+print 'Tweet!'
+print ''
+print tweet
+print ''
+
