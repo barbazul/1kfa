feedfe494a1197af4b91746ace725eaac488f8d0
Shandy Brown <github@ezide.com>
2019-04-03 16:16:46 -0700

    Attempt web publishing
    Former-commit-id: d99cd68d28676bf8edac26045c2e64e630e19ef2 [formerly 3427959f5f658dbc9358cd0aca65d4726fd11c6e]
    Former-commit-id: a457d5afac1554f4387c542ab13be7833181838a

--- bin/publish_web.sh
+++ bin/publish_web.sh
@@ -1,3 +1,18 @@
 #! /bin/bash
 
 echo "Publish Web"
+
+source $KFAREPO/resolution_cards/version.py
+cd /tmp/
+rm -rf deckahedron_site
+git clone https://github.com/sjbrown/deckahedron_site.git
+mkdir -p deckahedron_site/dist/$VERSION
+cd deckahedron_site/dist/$VERSION
+cp /tmp/*tar.gz ./
+git add .
+git config credential.helper 'cache --timeout=120'
+git config user.email "sjbrown@geeky.net"
+git config user.name "Circle CI Deployment Bot"
+git commit --allow-empty -m "Automatic Circle CI build $CIRCLE_BUILD_URL"
+git push -q https://${GITHUB_TOKEN}@github.com/sjbrown/deckahedron_site.git master
+
