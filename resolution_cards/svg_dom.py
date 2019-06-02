de33cf29dc2602595221f1b1855006e85d2b606d
Shandy Brown <github@ezide.com>
2019-06-02 12:25:08 -0700

    more doodads on svg_dom.py
    Former-commit-id: 7d8959577f27bd814dacbfdc62d7f790e6f73538 [formerly 04e0d1b7e758a8a730dcf41e7868a11386bade02]
    Former-commit-id: 0e05928e01b1864536aed093238c888532099636

--- resolution_cards/svg_dom.py
+++ resolution_cards/svg_dom.py
@@ -11,11 +11,16 @@ sys.path.append('/usr/share/inkscape/extensions/')
 from version import VERSION
 
 DEBUG = int(os.environ.get('DEBUG', 0))
+SINGLETON = object()
+XLINK='http://www.w3.org/1999/xlink'
 
 def run(cmd):
     if DEBUG:
         print cmd
     os.system(cmd)
+
+def just_basename(fpath):
+    return os.path.splitext(os.path.basename(fpath))[0]
 
 def ensure_dirs(filepath):
     if not os.path.isdir(os.path.dirname(filepath)):
@@ -128,8 +133,44 @@ def change_flowroot_text(flowroot, newtext, style, ideal_num_chars):
               flowroot.attrib['style']
             )
 
+def get_attrib(node, attr, default=SINGLETON):
+    for ns in [''] + node.nsmap.values():
+        key = '{%s}%s' % (ns, attr)
+        try:
+            x = node.attrib[key]
+        except:
+            continue
+        return x
+    if default == SINGLETON:
+        raise Exception('attribute %s not found in node %s' % (attr, node))
+    else:
+        return default
+
+def set_attrib(node, attr, val):
+    for ns in [''] + node.nsmap.values():
+        key = '{%s}%s' % (ns, attr)
+        try:
+            if val is None:
+                del node.attrib[key]
+            else:
+                node.attrib[key] = val
+            return
+        except KeyError:
+            continue
+    if val != None:
+        node.attrib[attr] = val
+
+def get_elems(node, tag):
+    for ns in [''] + node.nsmap.values():
+        key = '{%s}%s' % (ns, tag)
+        x = node.findall(key)
+        if x:
+            return x
+    raise Exception('elements "%s" not found in node %s' % (attr, node))
+
 class DOM(object):
     def __init__(self, svg_file):
+        self._local_dir = os.path.dirname(svg_file) or '.'
         fp = file(svg_file)
         c = fp.read()
         c = c.replace('VERSION', VERSION)
@@ -154,6 +195,89 @@ class DOM(object):
     def layer_show(self, layer_label):
         self.layers[layer_label].attrib['style'] = 'display:inline'
 
+    def layer_only_show(self, layer_label):
+        for current in self.layers:
+            if current == layer_label:
+                self.layers[current].attrib['style'] = 'display:inline'
+            else:
+                self.layers[current].attrib['style'] = 'display:none'
+
+    def svg_to_symbol(self, symbol_id):
+        svg_node = self.dom.getiterator().next()
+        print 'defs', get_elems(svg_node, 'defs')
+        [ svg_node.remove(e) for e in get_elems(svg_node, 'defs') ]
+        [ svg_node.remove(e) for e in get_elems(svg_node, 'namedview') ]
+        [ svg_node.remove(e) for e in get_elems(svg_node, 'metadata') ]
+        dom = etree.fromstring('<symbol x="0" y="0" width="10" height="10" />')
+        for elem in svg_node.getchildren():
+            svg_node.remove(elem)
+            dom.append(elem)
+        set_attrib(dom, 'id', symbol_id)
+        for elem in dom.getiterator():
+            old_id = get_attrib(elem, 'id', None)
+            if old_id:
+                set_attrib(elem, 'id', symbol_id + '_' + old_id)
+        return dom
+
+    def by_id(self, node_id):
+        for elem in self.dom.getiterator():
+            if get_attrib(elem, 'id', None) == node_id:
+                return elem
+        raise KeyError('ID not found: %s' % node_id)
+
+    def insert_layer_as_symbol(self, layer_name):
+        fpath, layer_label = uri.split('#')
+        if not os.path.isabs(fpath):
+            fpath = os.path.join(self._local_dir, fpath)
+        s_dom = DOM(fpath)
+        orig_width = get_attrib(s_dom.dom, 'viewBox').split()[2]
+        orig_height = get_attrib(s_dom.dom, 'viewBox').split()[3]
+        symbol = s_dom.layers[layer_label]
+        print ''
+        print 'symbol'
+        print '-------------------------'
+        print etree.tostring(symbol)
+        symbol_id = layer_label
+        symbol.attrib['id'] = symbol_id
+        set_attrib(symbol, 'x', '0')
+        set_attrib(symbol, 'y', '0')
+        set_attrib(symbol, 'label', None)
+        set_attrib(symbol, 'groupmode', None)
+        set_attrib(symbol, 'data-orig-width', orig_width)
+        set_attrib(symbol, 'data-orig-height', orig_height)
+        self.layers['symbols'].append(symbol)
+        return symbol_id
+
+    def replace_nodes_with_symbols(self, symbol_id):
+        symbol = self.by_id(symbol_id)
+        print 'syid', symbol_id
+        print 'keys', self.title_to_elements.keys()
+        for key in self.title_to_elements.keys():
+            if not (key.startswith('use-') and key.endswith(symbol_id)):
+                continue
+            for node in self.title_to_elements[key]:
+                print ''
+                print key
+                print '-------------------------'
+                print etree.tostring(node)
+                old_width = float(get_attrib(node, 'width'))
+                old_height = float(get_attrib(node, 'height'))
+                symbol_width = float(get_attrib(symbol, 'data-orig-width'))
+                symbol_height = float(get_attrib(symbol, 'data-orig-height'))
+                print '3333333333333'
+                print old_width, old_height, symbol_width, symbol_height
+                xpct = '%3.3f%%' % (symbol_width / old_width)
+                ypct = '%3.3f%%' % (symbol_height / old_height)
+                newnode = etree.SubElement(node.getparent(), 'use')
+                #'<use x="0" y="0" width="100%" height="100%" xlink:href="" />'
+                newnode.attrib['id'] = 'clone_' + symbol_id
+                newnode.attrib['{%s}href' % XLINK] = '#' + symbol_id
+                set_attrib(newnode, 'x', get_attrib(node, 'x'))
+                set_attrib(newnode, 'y', get_attrib(node, 'y'))
+                set_attrib(newnode, 'width', xpct)
+                set_attrib(newnode, 'height', ypct)
+                node.getparent().replace(node, newnode)
+
     def cut_element(self, title):
         for e in self.title_to_elements[title]:
             e.getparent().remove(e)
@@ -162,6 +286,12 @@ class DOM(object):
         e = self.layers[layer_label]
         if e.getparent():
             e.getparent().remove(e)
+
+    def add_layer(self, layerNode):
+        assert layerNode.tag == 'g'
+        label = layerNode.attrib['{http://www.inkscape.org/namespaces/inkscape}label']
+        self.dom.append(layerNode)
+        self.layers[label] = layerNode
 
     def replace_text(
         self,
