21bb1856a3bc805fc01e8310548d4170f3466715
Shandy Brown <github@ezide.com>
2020-03-11 14:25:11 -0700

    make card backgrounds white in the command-line app
    Former-commit-id: 1119c3e7e5e3405b270cca763916e840dfbd1816 [formerly da01113f358c6fe04eaeb99665cdf5ad11787007]
    Former-commit-id: 74930ba5450d087ffa36d585e53e01f5c0bff4d2

--- resolution_cards/cmdline_cards.py
+++ resolution_cards/cmdline_cards.py
@@ -12,15 +12,16 @@ crown = '♛'
 dragon = '♞'
 cardback = '🂠 '
 cardup = '🃟 '
-RED="\033[0;31m"
-BLU="\033[0;34m"
-BLU1="\033[1;34m"
-GRN="\033[0;32m"
-GRN1="\033[1;32m"
-YEL="\033[0;33m"
-YEL1="\033[1;33m"
+RED="31" #"\033[0;31m"
+BLU="34" # "\033[0;34m"
+GRN="32" #"\033[0;32m"
+YEL="33" # "\033[0;33m"
 NOCOLOR="\033[0m"
+#WHT= "\[\033[0;38m\]"   ;  WHT1="\[\033[1;38m\]"  #white
+#BWHT="\[\033[0;45m\]"  ;  BWHT1="\[\033[1;48m\]"  #white
 
+COL_FMT = "\033[%s;%sm"
+BG = "0"
 
 p1 = {
   'deck': deck,
@@ -40,22 +41,37 @@ def discard():
     [p1['discard'].append(card) for card in p1['resolving']]
     p1['resolving'] = []
 
+def reshuffle():
+    discard()
+    p1['deck'] = p1['deck'] + p1['discard']
+    p1['discard'] = []
+    shuffle(p1['deck'])
+
+
 
 def red(s):
-    return RED + s + NOCOLOR
+    return COL_FMT % (BG, "31") + s #+ NOCOLOR
 def green(s):
-    return GRN + s + NOCOLOR
+    return COL_FMT % (BG, GRN) + s #+ NOCOLOR
 def yellow(s):
-    return YEL + s + NOCOLOR
+    return COL_FMT % (BG, YEL) + s #+ NOCOLOR
 def blue(s):
-    return BLU + s + NOCOLOR
+    return COL_FMT % (BG, BLU) + s #+ NOCOLOR
+def white_bg_on():
+    global BG
+    BG = "47"
+def white_bg_off():
+    global BG
+    BG = "0"
 
 def face(card):
+    white_bg_on()
     a = red('%s %s' % (anvil, card['a']))
     b = blue('%s %s' % (blades, card['b']))
     c = yellow('%s %s' % (crown, card['c']))
     d = green('%s %s' % (dragon, card['d']))
-    return ' '.join([a, b, c, d])
+    white_bg_off()
+    return ' '.join([a, b, c, d]) + NOCOLOR
 
 def print_state():
   deck = cardback + 'x%s' % len(p1['deck'])
@@ -72,11 +88,15 @@ try:
         print('')
         print_state()
         print('')
-        command = raw_input('flip or discard ? > ')
-        if command[0].lower() == 'f':
-            flip()
-        if command[0].lower() == 'd':
-            discard()
+        command = raw_input('flip, discard, reshuffle ? > ')
+        command = command.lower() or 'X'
+        fn = {
+            'X': lambda: None,
+            'f': flip,
+            'd': discard,
+            'r': reshuffle,
+        }[command[0]]
+        fn()
 
 except (EOFError, KeyboardInterrupt):
     print('\nEND\n')
